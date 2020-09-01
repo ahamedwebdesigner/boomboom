8adb9f2e7bd650b777cda5bc276a6ade09b762b1
ahamemd.webdesigner@gmail.com <ahamemd.webdesigner@gmail.com>
2020-09-01 21:58:01 +0530

    rendering views

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'myview'));
+
 
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+   // res.send('Hello World : ');
+   res.render('index',{title:"First node application"});
 })
+
+app.get('/product', (req,res)=>{res.render('product',{
+                                        title:"THis is products page",
+                                        description:'THis is all about products '
+                                    })})
+
 
 module.exports = app;
