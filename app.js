37b6d59e5159859a89f5181a577e9c7056e86d7f
ahamemd.webdesigner@gmail.com <ahamemd.webdesigner@gmail.com>
2020-09-07 15:26:43 +0530

    working with data

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 var path = require('path');
 var appconfig = require('./config');
 
+
+app.use(express.urlencoded());
+app.use(express.json());      // if needed
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'myview'));
 
@@ -17,6 +21,69 @@ app.get('/product', (req,res)=>{res.render('product',{
                                         description:'THis is all about products '
                                     })})
 
+app.get('/test',function(request,response){
+
+    console.log("--------------------------------------------");
+    //console.log(request.headers);
+    console.log(request.query);
+     
+    console.log("--------------------------------------------");
+
+    response.render('index',{title:appconfig.appName})
+})
+
+
+
+
+app.get('/student/:id/:message',(req,res)=>{
+
+    let students = [
+        {name:'arshiya'  , place:'Bhagyanagar '   }, //0
+        {name: 'shabana' , place:'Sainagar '  },
+        {name: 'reshma'  , place:'Usa '  },
+        {name: 'Zabiulla', place:'Bangalore '  }
+    ];
+
+    let messages={
+        hellow:'Hellow how are you',
+        bye:"I am very sad to say good bye"
+    }
+      
+    console.log("--------------------------------------------");
+    console.log(req.params.id);
+    console.log("--------------------------------------------");
+    var stuid;
+    if(req.params.id>students.length-1){
+         stuid = 0;
+    }else{
+        stuid = req.params.id;
+    }
+    res.render('student',{
+                    title:appconfig.appName,
+                    studentId: students[stuid],
+                    message: messages[req.params.message]
+                });
+});
+
+
+
+app.get('/create-student',(req,res)=>{
+    res.render('studentform',{
+        title:appconfig.appName
+     });
+})
+app.post('/save-student',(req,res)=>{
+
+    console.log("--------------------------------------------");
+    console.log( req.body);
+    console.log("--------------------------------------------");
+
+    res.render('studentSaved',{
+        title:appconfig.appName,
+        studentName:req.body.studentName
+     });
+    //res.sendStatus(200);
+})
 
 
 
